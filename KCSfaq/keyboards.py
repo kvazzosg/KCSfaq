4e080e7de1f6657fd236a1a649ee98fd9539067b
Kvazz <166524911+kvazzosg@users.noreply.github.com>
2024-04-18 22:23:44 +0200

    1.0

---
+++ KCSfaq/keyboards.py
@@ -0,0 +1,17 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+
+help_b = KeyboardButton("/help")
+about_b = KeyboardButton("О боте")
+vopros_b = KeyboardButton("FAQ")
+
+menu_kb.add( vopros_b, help_b, about_b)
+
+
+menu_kb2 = InlineKeyboardMarkup(row_width=1)
+
+channel_b = InlineKeyboardButton(text="Подписаться на канал разработчиков", url="https://t.me/KomaruCatStudios")
+bot_b = InlineKeyboardButton(text="Не нашли нужный вопрос?", url="http://t.me/KomaruStudios_bot")
+
+menu_kb2.add(channel_b,bot_b)
---
+++ KCSfaq/main.py
@@ -0,0 +1,93 @@
+from aiogram import Bot,Dispatcher,types, executor
+from aiogram.types import InputFile
+from config import TOKEN
+from text import *
+from keyboards import *
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+startpic = InputFile("KCSlogo.png")
+voprospic = InputFile("chast_vopros.png")
+skrinpic = InputFile("rofl.png")
+pomoshpic = InputFile("pomosh.png")
+o_botepic = InputFile("o_bote.png")
+otvpic = InputFile("logo_otv14.png")
+
+@dp.message_handler(commands="start")
+async def hello(msg:types.Message):
+    await bot.send_photo(chat_id=msg.chat.id, photo=startpic, caption=start, reply_markup=menu_kb)
+
+@dp.message_handler(text="FAQ")
+async def faqdef(msg:types.Message):
+    await bot.send_photo(chat_id=msg.chat.id, photo=voprospic, caption=faq)
+
+@dp.message_handler(text="1")
+async def otv1def(msg:types.Message):
+    await msg.answer(otv1)
+
+@dp.message_handler(text="2")
+async def otv2def(msg:types.Message):
+    await msg.answer(otv2)
+
+@dp.message_handler(text="3")
+async def otv3def(msg:types.Message):
+    await bot.send_photo(chat_id=msg.chat.id, photo=skrinpic, caption=otv3)
+
+@dp.message_handler(text="4")
+async def otv4def(msg:types.Message):
+    await msg.answer(otv4)
+
+@dp.message_handler(text="5")
+async def otv5def(msg:types.Message):
+    await msg.answer(otv5)
+
+@dp.message_handler(text="6")
+async def otv6def(msg:types.Message):
+    await msg.answer(otv6)
+
+@dp.message_handler(text="7")
+async def otv7def(msg:types.Message):
+    await msg.answer(otv7)
+
+@dp.message_handler(text="8")
+async def otv8def(msg:types.Message):
+    await msg.answer(otv8)
+
+@dp.message_handler(text="9")
+async def otv9def(msg:types.Message):
+    await msg.answer(otv9)
+
+@dp.message_handler(text="10")
+async def otv10def(msg:types.Message):
+    await msg.answer(otv10)
+
+@dp.message_handler(text="11")
+async def otv11def(msg:types.Message):
+    await msg.answer(otv11)
+
+@dp.message_handler(text="12")
+async def otv12def(msg:types.Message):
+    await msg.answer(otv12)
+
+@dp.message_handler(text="13")
+async def otv13def(msg:types.Message):
+    await msg.answer(otv13)
+
+@dp.message_handler(text="14")
+async def otv14def(msg:types.Message):
+    await bot.send_photo(chat_id=msg.chat.id, photo=otvpic, caption=otv14)
+
+@dp.message_handler(text="15")
+async def otv15def(msg:types.Message):
+    await msg.answer(otv15)
+
+@dp.message_handler(commands="help")
+async def help(msg:types.Message):
+    await bot.send_photo(chat_id=msg.chat.id, photo=pomoshpic, caption=helpdef)
+
+@dp.message_handler(text="О боте")
+async def avtor_b(msg:types.Message):
+    await bot.send_photo(chat_id=msg.chat.id, photo=o_botepic, caption=avtor)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
